d43d8e20522de7f425c639edc3e0f116ea5efd85
Noah.lagerström <noah.lagerstrom@elev.ga.ntig.se>
2022-10-06 13:17:13 +0200

    Updated read more buttons

--- js/readmore.js
+++ js/readmore.js
@@ -13,3 +13,35 @@ function Readmore() {
       moreText.style.display = "inline";
     }
 }
+
+function Readmore2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Läs mer";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Läs mindre";
+    moreText2.style.display = "inline";
+  }
+}
+
+function Readmore3() {
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText3 = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "Läs mer";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "Läs mindre";
+    moreText3.style.display = "inline";
+  }
+}
