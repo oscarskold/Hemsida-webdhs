8ef89e1f105c93c7df188c0ebed64ea36d10711c
Vilgot Thyrén <Vilgot.thyren@elev.ga.ntig.se>
2022-11-08 10:27:23 +0100

    Revert "start"
    This reverts commit cfd64fd0797376202ccc778e4359ba9d2982703f.

---
+++ prototyp/header prototyp 2/app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.navbar_links')
+    const navLinks = document.querySelectorAll('.navbar_links li, a')
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+            //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ''
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger Animation             
+        burger.classList.toggle('toggle');    
+    });
+
+}
+
+const header = document.querySelector('.header')
+
+navSlide();
