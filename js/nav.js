0923a61f8504bae6246ccd63a825e75e66ce48ae
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 23:03:32 +0100

    moved nav code to js file, removed lorem ipsum, added text from instagram in all product pages

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,71 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+      <nav>
+      <div class="logo nav-pos">
+          <a href="Homepage.html"><img src="../img/logo-white.png" alt=""><h3>Web-DHS</h3></a>
+      </div>
+      <div class="r-side">
+          <a href="shop.html" class="nav-items"><img src="../img/icons8-shopping-cart-32.png" alt=""></a>
+          <div class="burger right nav-items">
+              <div class="line1"></div>
+              <div class="line2"></div>
+              <div class="line3"></div>
+          </div>
+      </div>    
+      <ul class="navbar_links nav">
+          <li>
+              <a href="Homepage.html">Start</a>
+          </li>
+          <li>
+              <a href="About.html">Om oss</a>
+          </li>
+          <li>
+              <a href="Services.html">Tjänster</a>
+          </li>
+          <li>
+              <a href="#footer">Kontakt</a>
+          </li>
+      </ul>
+      
+      </nav>
+      `;
+    }
+}
+  
+customElements.define('nav-component', Header);
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.navbar_links')
+    const navLinks = document.querySelectorAll('.navbar_links li')
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');   
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ''
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger Animation             
+        burger.classList.toggle('toggle');    
+    });
+
+}
+
+const header = document.querySelector('.header')
+
+navSlide();
+
 const nav = document.querySelector('nav');
 
         window.addEventListener('scroll', () => {
@@ -6,4 +74,4 @@ const nav = document.querySelector('nav');
             } else {
                 nav.classList.remove('active_nav');
             }
-        })
+})
