2da3e4ce1b977625e9a157994490fd677912a358
Vilgot Thyrén <Vilgot.thyren@elev.ga.ntig.se>
2022-12-06 22:07:52 +0100

    yes

--- old/js/SQL/cart/cart.js
+++ old/js/SQL/cart/cart.js
@@ -1,14 +1,13 @@
 // document.getElementById('test').innerHTML = result;
-var mysql = require('mysql')
 var con = require('./config/database.js')
 // var Connection = mysql.createConnection(con)
 
 
 
 
-con.query('SELECT name, description, price FROM produkt', function(err, result, fields){
+con.query('SELECT name FROM produkt', function(err, result, fields){
     if (err) throw err
-    console.log(result)
+    console.log(result[0])
     
 })
 
