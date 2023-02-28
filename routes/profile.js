cdde87980fbc30c0a7974276605e298403236701
Noah.lagerström <noah.lagerstrom@elev.ga.ntig.se>
2023-02-28 16:07:40 +0100

    Added back-end to show user name on profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,20 @@
+var con = require('../SQL/cart/config/database.js');
 var express = require('express');
+const session = require ('express-session')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('profile', { title: '' });
+  var user = req.session.userid
+  con.query (`SELECT * FROM users WHERE user_name='${user}'`, function(err, result){
+    if (err){
+      throw err
+    } else {
+      console.log(result)
+      res.render('profile', { title: '', data: result });
+    }
+  })
 });
 
 module.exports = router;
+  
