850a988a77f19493165f8d0d7fcba36fa5c7aaae
Vilgot <vilgot.thyren@gmail.com>
2023-05-04 21:14:34 +0200

    more bug fix about file name

--- routes/Baspaket.js
+++ routes/Baspaket.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Baspaket', { title: '' });
+  res.render('baspaket', { title: '' });
 });
 
 module.exports = router;
